1584998a5517c612222e358b10452d2ff7574a57
jhoaking <wawxper08@gmail.com>
2025-03-17 09:45:58 -1100

    .gitignore

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,5 @@
 import pg from 'pg'
-import {DB_USER,DB_DATABASE,DB_HOST,DB_PASSWORD,DB_PORT,PORT} from '../confing.js'
+import {DB_USER,DB_DATABASE,DB_HOST,DB_PASSWORD,DB_PORT} from '../confing.js'
 
 export const pool = new pg.Pool({
     user:DB_USER,
